65279ce11c9e8e60fd484e3623bd0e1d1448970a
Alexander Rodriguez <criozone@ukr.net>
2018-07-22 20:12:34 +0200

    Close tasks implement

--- js/options.js
+++ js/options.js
@@ -22,6 +22,7 @@ $(function(){
             if (chromStorage.oldSheets) {
                 var sheetsList = '';
                 chromStorage.oldSheets.forEach(function(item, i, arr){
+                    if (!item) return;
                     sheetsList += '<li><a href="' + item.link + '" target="_blank">' + item.name + '</a><span class="delete-sheet" data-index="' + i + '"></span></li>';
                 });
                 $('#sheets-list').html(sheetsList);
@@ -97,16 +98,126 @@ $(function(){
         });
     });
 
+    var taskUrlColumn = null;
+    var currentSheet = null;
 
-
-    /* 
-        TODO:
-        1) Handle "close-tasks"
-        2) Select sheet to edit("timsheet", "summary", "Правильное заполнение")
-        3) Индикатор загрузки/работы (load.gif)
-    */
     $(document).on("click", '#close-tasks-button', function(){
-        // This is just a test:
-		console.log('Does nothing yet.');
+        chrome.storage.sync.get("currentSheet", function(chromStorage){
+            if (!chromStorage.currentSheet) {
+                console.log('Error loading current sheet');
+                return;
+            }
+            currentSheet = chromStorage.currentSheet;
+            var range = 'timesheet';
+            chrome.identity.getAuthToken({interactive: true}, function(token){
+                let init = {
+                    method: 'GET',
+                    async: true,
+                    headers: {
+                        Authorization: 'Bearer ' + token,
+                        'Content-Type': 'application/json'
+                    },
+                    'contentType': 'json'
+                };
+                fetch(
+                    'https://sheets.googleapis.com/v4/spreadsheets/' + currentSheet.spredsheetId + '/values/' + range + '?valueRenderOption=FORMATTED_VALUE&key=' + API_KEY,
+                    init
+                )
+                .then((response) => response.json())
+                .then(function(data) {
+                    if (!data.hasOwnProperty('values') || data.values.length < 1) return;
+                    var sheet = data.values;
+                    taskUrlColumn = sheet[0].length -1;
+                    var filledByDefaultRowsOffset = 2;
+                    var shortCardIds = [];
+
+                    for (let i = filledByDefaultRowsOffset; i < sheet.length; i++) {
+                        if (typeof sheet[i][taskUrlColumn] !== 'undefined') {
+                            shortCardIds.push(
+                                {
+                                    "id": (sheet[i][taskUrlColumn].match(/trello\.com\/.+\/(.+)\/\d+.+/i))[1],
+                                    "row": i
+                                }
+                            );
+                        }
+                    }
+
+                    for (let i  = 0; i < shortCardIds.length; i++) {
+                        checkTask(shortCardIds[i]);
+                    }
+                });
+            });
+        });
     });
+
+    var checkTask = function(shortTaskData, spreadsheetIndex) {
+        let init = {
+            method: 'GET',
+            async: true,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            'contentType': 'json'
+        };
+        fetch(
+            'https://api.trello.com/1/cards/' + shortTaskData.id + '/list?key=' + trelloPublicKey + '&token=' +  trelloToken,
+            init
+        ).then((response) => response.json()).then(function(data){
+            var closedFlag = false;
+            if (data.name.search('Done') > -1) {
+                closedFlag = true;
+            } else if (data.name.search(/Documentation\s+\(Teo\)/i) > -1) {
+                closedFlag = true;
+            } else if (data.name.search(/Ready\s+for\s+Live\s+\(BJ\)/i) > -1) {
+                closedFlag = true;
+            }
+            if (closedFlag)
+                closeTask(shortTaskData.row);
+        });
+    }
+    var closeTask = function(rowIndex){
+        var totalHoursColumn = taskUrlColumn - 2;
+        var repeatCell = {
+            range: {
+				sheetId: currentSheet.sheetId,
+				startRowIndex: rowIndex,
+				endRowIndex: rowIndex + 1,
+				startColumnIndex: totalHoursColumn,
+				endColumnIndex: totalHoursColumn + 1
+			},
+			cell: {
+				userEnteredFormat: {
+					backgroundColor: {
+						red: 182,
+						green: 215,
+						blue: 168,
+						alpha: 1
+					}
+				}
+			},
+			fields: "userEnteredFormat.backgroundColor"
+        };
+        chrome.identity.getAuthToken({interactive: true}, function(token){
+            let init = {
+                method: 'POST',
+                async: true,
+                headers: {
+                    Authorization: 'Bearer ' + token,
+                    'Content-Type': 'application/json'
+                },
+                'contentType': 'json',
+                body: JSON.stringify({
+                    requests: [
+                        {repeatCell: repeatCell}
+                    ]
+                })
+            };
+            fetch(
+                'https://sheets.googleapis.com/v4/spreadsheets/' + currentSheet.spredsheetId + ':batchUpdate?key=' + API_KEY,
+                init
+            ).then((response) => response.json()).then(function(markData){
+                console.log(markData);
+            });
+        });
+    };
 });
